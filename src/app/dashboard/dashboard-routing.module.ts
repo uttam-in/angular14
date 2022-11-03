4894242c2339cd3cd92a68528411e52d9bed4aee
Uttam Kumar P <panasalauttam@gmail.com>
2022-11-03 23:58:02 +0530

    routings

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,15 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ComprasComponent } from './compras/compras.component';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { ProveedorComponent } from './proveedor/proveedor.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 const routes: Routes = [
   {
     path: '', component: DashboardComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'inventario',
+        component: InventarioComponent
+      },
+      {
+        path: 'proveedor',
+        component: ProveedorComponent
+      },
+      {
+        path: 'clientes',
+        component: ClientesComponent
+      },
+      {
+        path: 'ventas',
+        component: VentasComponent
+      },
+      {
+        path: 'compras',
+        component: ComprasComponent
       }
     ]
   }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,22 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { ProveedorComponent } from './proveedor/proveedor.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { ComprasComponent } from './compras/compras.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    InventarioComponent,
+    ProveedorComponent,
+    ClientesComponent,
+    VentasComponent,
+    ComprasComponent
   ],
   imports: [
     CommonModule,
