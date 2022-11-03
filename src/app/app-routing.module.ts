67145f1214b03cddf432868476c0418c9319391d
Uttam Kumar P <panasalauttam@gmail.com>
2022-11-03 22:32:15 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'login', 
-  loadChildren: () => 
-  import('./login/login.module').then(m => m.LoginModule) }, 
-  { path: 'dashboard', 
-  loadChildren: () => 
-  import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  }
 ];
 
 @NgModule({
