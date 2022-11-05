8a19d694c8b8451e1d74db14ce9ce13be936328e
Uttam Kumar P <panasalauttam@gmail.com>
2022-11-05 12:19:40 +0530

    token

--- src/app/services/token.interceptor.ts
+++ src/app/services/token.interceptor.ts
@@ -3,15 +3,17 @@ import {
     HttpRequest,  
     HttpHandler,  
     HttpEvent,  
-    HttpInterceptor  
+    HttpInterceptor,  
+    HttpResponse
 } from '@angular/common/http';  
  
-import { Observable } from 'rxjs/internal/Observable';
-
+import { Observable } from 'rxjs';
+import { NgxSpinnerService } from "ngx-spinner";
+import { tap } from 'rxjs/operators';
   
-  @Injectable()  
-  export class TokenInterceptor implements HttpInterceptor {  
-    constructor() {}  
+@Injectable()  
+export class TokenInterceptor implements HttpInterceptor {  
+    constructor(private spinner: NgxSpinnerService) {}  
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {  
         
       request = request.clone({  
@@ -19,6 +21,26 @@ import { Observable } from 'rxjs/internal/Observable';
           Authorization: `Bearer ${localStorage.getItem('token')}`  
         }  
       });    
-      return next.handle(request);  
+      return next.handle(request).pipe(
+        tap(
+          event => this.handleResponse(request, event),
+          error => this.handleError(request, error)
+        )
+      );
     }  
+
+    handleResponse(req: HttpRequest<any>, event: any) {
+      //console.log('Handling response for ', req.url, event);
+      if (event instanceof HttpResponse) {
+        // console.log('Request for ', req.url,
+        //     ' Response Status ', event.status,
+        //     ' With body ', event.body);
+      }
+    }
+  
+    handleError(req: HttpRequest<any>, event:any) {
+      // console.error('Request for ', req.url,
+      //       ' Response Status ', event.status,
+      //       ' With error ', event.error);
+    }
   }  
