489036c2d180ac0032e207dd470f73929a2bb96a
Uttam Kumar P <panasalauttam@gmail.com>
2022-11-09 22:06:42 +0530

    create

--- src/app/dashboard/clientes/clientes.component.ts
+++ src/app/dashboard/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Cliente } from 'src/app/models/cliente';
 import { ClienteService } from 'src/app/services/cliente.service';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-clientes',
@@ -17,7 +17,14 @@ export class ClientesComponent implements OnInit {
   page:number = 1;
   config: any;
 
-  constructor(private clienteService: ClienteService) { }
+  @ViewChild("clientesModal")
+  modalContent!: TemplateRef<any>;
+
+  closeResult: string = '';
+
+  constructor(private clienteService: ClienteService,
+    private modalService: NgbModal
+    ) { }
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe(
@@ -34,4 +41,28 @@ export class ClientesComponent implements OnInit {
   pageChanged(event: any){
     this.config.currentPage = event;
   }
+
+  open() {
+    this.modalService.open(this.modalContent, {ariaLabelledBy: 'modal-basic-title', size: <any>'xl' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  } 
+
+  close(){
+    this.modalService.dismissAll()
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+
 }
